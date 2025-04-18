90be2e2e446a7ed776906c91c5c301be5a8d7097
avi-codesmith <avi.codesmith@gmail.com>
2025-04-18 20:27:23 +0530

    work in progressing

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
 "use strict";
 
 const mainFunction = async () => {
-  let a = await fetch();
+  let a = await fetch("http://127.0.0.1:5500/songs/");
+  let response = await a.text();
+  let div = document.createElement("div");
+  div.innerHTML = response;
+  let as = div.getElementsByTagName("a");
+  let songs = [];
+  for (let i = 0; i < as.length; i++) {
+    const element = as[i];
+    if (element.href.endsWith(".mp3")) {
+      songs.push[element.href];
+      console.log("have an mp3");
+    }
+  }
+  console.log(songs);
 };
+
+mainFunction();
